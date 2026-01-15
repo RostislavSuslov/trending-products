15fd72d14284541554898052a4453347096c8d84
RostSuslov <suslovros@gmail.com>
2026-01-15 08:53:32 +0200

    feat: create html layout

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import postcss from 'postcss'
 import { resolve } from "path";
-import handlebars from 'vite-plugin-handlebars';
-import FullReload from 'vite-plugin-full-reload'
 
 export default defineConfig({
   base: '/',
@@ -29,12 +26,4 @@ export default defineConfig({
       }
     }
   },
-  
-  plugins: [
-    tailwindcss(),
-    handlebars({
-      partialDirectory: resolve(__dirname, "src/partials")
-    }),
-    FullReload(["src/partials/**/*"])
-  ],
 })
