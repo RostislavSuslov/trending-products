5cbc5a7ba38111acf4710ec3f546a8114b9dc8ff
RostSuslov <suslovros@gmail.com>
2026-01-15 16:41:23 +0200

    feat: add sale modifier class to product badges

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import 'reset-css/reset.css';
 import './main.scss'
 import './style.css'
+
+const productBadges = document.querySelectorAll('.product__badge-item')
+
+productBadges.forEach(item => {
+    item.textContent == 'Sale' || 'sale' ? item.classList.add('product__badge-item--sale') : null
+})
